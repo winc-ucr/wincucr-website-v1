12d608358a588cdf5b16089744640ac1e25ce82c
Anchita Bora <anchita.bora@gmail.com>
2020-08-29 14:31:37 -0700

    Initial commit from gatsby: (https://github.com/ajayns/gatsby-absurd.git)

---
+++ src/components/common/SEO.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
+const SEO_DATA = {
+  description: 'An absurd gatsby starter',
+  title: 'Absurd',
+  url: '',
+  author: 'ajayns',
+  keywords: ['gatsby', 'site', 'absurd'],
+  img: '',
+  twitterId: '@ajayns_',
+  facebookId: '',
+};
+
+const SEO = () => {
+  return (
+    <Helmet>
+      <meta property="fb:app_id" content={SEO_DATA.facebookId} />
+      <meta property="og:title" content={SEO_DATA.title} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={SEO_DATA.url} />
+      <meta property="og:image" content={SEO_DATA.img} />
+      <meta property="og:description" content={SEO_DATA.description} />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content={SEO_DATA.twitterId} />
+      <meta name="twitter:site" content={SEO_DATA.url} />
+      <meta name="twitter:title" content={SEO_DATA.title} />
+      <meta name="twitter:description" content={SEO_DATA.description} />
+      <meta name="twitter:domain" content={SEO_DATA.url} />
+      <meta name="twitter:image:src" content={SEO_DATA.img} />
+
+      <meta name="description" content={SEO_DATA.description} />
+      <meta name="keywords" content={SEO_DATA.keywords.join(', ')} />
+      <meta name="author" content={SEO_DATA.author} />
+      <title>{SEO_DATA.title}</title>
+      <html lang="en" />
+    </Helmet>
+  );
+};
+
+export default SEO;
